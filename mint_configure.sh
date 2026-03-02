fc0785fe7c4856ca17a77c4dbe2d290e81f014c3
user <user@gamil.com>
2026-03-02 20:17:32 +0200

    Add poetry

--- mint_configure.sh
+++ mint_configure.sh
@@ -15,7 +15,8 @@ sudo apt install wireshark -y
 sudo add-apt-repository -y ppa:phoerious/keepassxc && sudo apt update && sudo apt install -y keepassxc
 ###############  UV #####################
 curl -LsSf https://astral.sh/uv/install.sh | sh
-
+############## POETRY ###################
+curl -sSL https://install.python-poetry.org | python3 -
 ########################    GOOGLE CHROME ####################################
 curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb && rm -f goo*.deb
 
